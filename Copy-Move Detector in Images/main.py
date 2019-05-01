8415bf67e90b2b2842ffc3a5b2f8d30366f68fc3
Ali Emre Keskin <aliemrekeskin@outlook.com>
2019-05-01 18:51:27 +0300

    ...

--- Copy-Move Detector in Images/main.py
+++ Copy-Move Detector in Images/main.py
@@ -3,17 +3,20 @@ import cv2
 import CopyMoveDetect
 
 #loading source image
-img=cv2.imread("img\Hats.bmp",cv2.IMREAD_GRAYSCALE)
+img=cv2.imread("img\TP_C01_006_copy.png",cv2.IMREAD_GRAYSCALE)
 height, width=img.shape[:2]
 cv2.imshow("Img",img)
 cv2.waitKey(1)
 
-result=np.copy(img)
+#result=np.copy(img)
+result=np.zeros((height,width),np.uint8)
 
-CopyMoveDetect.BlockMatchDCT(img,result,8,0,16,0,4,100,100)
+CopyMoveDetect.BlockMatchDCT(img,result,8,0,16,0,2,50,150)
 
 cv2.imshow("result",result)
 cv2.waitKey(1)
+
+cv2.imwrite("result.png",result)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
