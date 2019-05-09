bdec053e7ebe708999b83ed53cef2b2a65024b51
Ali Emre Keskin <aliemrekeskin@outlook.com>
2019-05-09 22:45:30 +0300

    finals

--- Copy-Move Detector in Images/CopyMoveDetect.py
+++ Copy-Move Detector in Images/CopyMoveDetect.py
@@ -5,6 +5,25 @@ from operator import itemgetter
 from math import sqrt
 import itertools
 
+def zigzagScan(matrix):
+    rows, columns=matrix.shape[:2]
+    
+    solution=[[] for i in range(rows+columns-1)] 
+  
+    for i in range(rows): 
+        for j in range(columns): 
+            sum=i+j 
+            if(sum%2 ==0): 
+  
+                #add at beginning 
+                solution[sum].insert(0,matrix[i][j]) 
+            else: 
+  
+                #add at end of the list 
+                solution[sum].append(matrix[i][j]) 
+
+    return solution
+
 def exactBlockMatch(src,dst,blockSize):
     #loading source image
     img=src
@@ -43,86 +62,6 @@ def exactBlockMatch(src,dst,blockSize):
         w1=int(i[1]%(width-B+1))
         dst[h1:h1+B,w1:w1+B]=blueBlock
 
-def BlockMatch(src,dst,blockSize,differenceThreshold,shiftSizeThreshold,shiftCountThreshold):
-    #loading source image
-    img=src
-    height, width=img.shape[:2]
-
-    #creating matrix A
-    B=blockSize
-    A=[]
-    indexInA=0
-    for row in range(0,height-B+1):
-        for col in range(0,width-B+1):
-            roi=img[row:row+B,col:col+B]
-            rowInA=[list(j) for sub in roi for j in sub]
-            A.append([indexInA,rowInA])
-            indexInA+=1
-
-    #sorting matrix A
-    sortedA=sorted(A, key=itemgetter(1))
-
-    #matching same elements in matrix A
-    matchIndexes=[]
-    for i in range(len(sortedA)-1):
-        #vec1=sortedA[i][1]
-        ##vec1 = [[1,2,3],[4,5,6], [7], [8,9]]
-        #vec1flat = list(itertools.chain.from_iterable(vec1))
-        #vec2=sortedA[i+1][1]
-        #vec2flat = list(itertools.chain.from_iterable(vec2))
-
-        #diff=False
-        #for j in range(len(vec1flat)):
-        #    temp=vec1flat[j]-vec2flat[j]
-        #    if(temp>differenceThreshold):
-        #        diff=True
-        #        break
-        #if(not diff):
-        #    matchIndexes.append([sortedA[i][0],sortedA[i+1][0]])
-
-
-        vec1=sortedA[i][1]
-        vec2=sortedA[i+1][1]
-        diff=0
-        for j in range(len(vec1)):
-            pix1=vec1[j]
-            pix2=vec2[j]
-            r_diff=int(pix1[0])-int(pix2[0])
-            g_diff=int(pix1[1])-int(pix2[1])
-            b_diff=int(pix1[2])-int(pix2[2])
-            if(r_diff>differenceThreshold):
-                diff+=1
-            if(g_diff>differenceThreshold):
-                diff+=1
-            if(b_diff>differenceThreshold):
-                diff+=1
-        if(diff==0):
-            matchIndexes.append([sortedA[i][0],sortedA[i+1][0]])
-
-    
-    #reduction
-    shiftVectorSizes=[]
-    for i in matchIndexes:
-        h0=int(i[0]/(width-B+1))
-        w0=int(i[0]%(width-B+1))
-        h1=int(i[1]/(width-B+1))
-        w1=int(i[1]%(width-B+1))
-        shiftVectorSizes.append(sqrt((h0-h1)**2+(w0-w1)**2))
-    print(shiftVectorSizes)
-
-    #marking matches
-    redBlock=np.zeros((B,B,3),np.uint8)
-    redBlock[:,:]=(0,0,255)
-    blueBlock=np.zeros((B,B,3),np.uint8)
-    blueBlock[:,:]=(255,0,0)
-    for i in matchIndexes:
-        h0=int(i[0]/(width-B+1))
-        w0=int(i[0]%(width-B+1))
-        dst[h0:h0+B,w0:w0+B]=redBlock
-        h1=int(i[1]/(width-B+1))
-        w1=int(i[1]%(width-B+1))
-        dst[h1:h1+B,w1:w1+B]=blueBlock
-
 def BlockMatchDCT(src,dst,blockSize,DCT_ReduceCount,q,compareCount,differenceThreshold,shiftSizeThreshold,shiftCountThreshold):
     height,width=src.shape[:2]
 
@@ -133,23 +72,18 @@ def BlockMatchDCT(src,dst,blockSize,DCT_ReduceCount,q,compareCount,differenceThr
     for row in range(0,height-B+1):
         for col in range(0,width-B+1):
             roi=src[row:row+B,col:col+B]    #cut roi from image
-            roi_f=np.float32(roi)       #convert to float
+            roi_f=np.float32(roi)           #convert to float
             dct=cv2.dct(roi_f)              #apply DCT
-            dctReduced=dct[:4,:4]           #reduce
-            dctReduced1D=[j for sub in dctReduced for j in sub]     #flatten
+            #dctReduced=dct[:4,:4]           #reduce Square
+            zigzag2D = zigzagScan(dct)        #reduce Zigzag
+            zigzag1D=[j for sub in zigzag2D for j in sub]     #flatten
+            dctReduced1D=zigzag1D[:DCT_ReduceCount]     #reduce
             quantized=[np.int32(j/q) for j in dctReduced1D]         #quantization
             A.append([indexInA,quantized])
             indexInA+=1
 
-    print(A[0])
     #sorting matrix A
     sortedA=sorted(A, key=itemgetter(1))
-
-    ##matching same elements in matrix A
-    #matchIndexes=[]
-    #for i in range(len(sortedA)-1):
-    #    if(sortedA[i][1]==sortedA[i+1][1]):
-    #        matchIndexes.append([sortedA[i][0],sortedA[i+1][0]])
 
     #matching same elements in matrix A
     matchIndexes=[]
--- Copy-Move Detector in Images/main.py
+++ Copy-Move Detector in Images/main.py
@@ -1,22 +1,74 @@
 import numpy as np
 import cv2
 import CopyMoveDetect
+import argparse
+import os
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", required=True)
+parser.add_argument("-o", "--output", required=False, default="result.png")
+parser.add_argument("-b", "--blockSize", required=False, default=8, type=int)
+parser.add_argument("-r", "--reduce", required=False, default=16, type=int)
+parser.add_argument("-q", "--quantization", required=False, default=16, type=int)
+parser.add_argument("-cc", "--compareCount", required=False, default=1, type=int)
+parser.add_argument("-d", "--differenceThreshold", required=False, default=2, type=int)
+parser.add_argument("-n", "--shiftNormThreshold", required=False, default=50, type=int)
+parser.add_argument("-c", "--shiftCountThreshold", required=False, default=150, type=int)
+args = parser.parse_args()
+
+intro = """
+             :oyhddhddhys+:
+           sdmmhhhhyhhhdmmdy+-
+         smhshhdhysoosyhhhhsdy+	 	$$\   $$\ $$$$$$$$\ $$\   $$\ 
+        hmdydho:.`    `.:ydhhmh+ 	$$ | $$  |\__$$  __|$$ |  $$ |
+       ymdyds/`          `/dhhmh+	$$ |$$  /    $$ |   $$ |  $$ |
+       mhsms/`            `+myhmo	$$$$$  /     $$ |   $$ |  $$ |
+       myyms- `hddy  hddy` :mhsms	$$  $$<      $$ |   $$ |  $$ |
+       mmymy/ .mydy  mydy` +msmms	$$ |\$$\     $$ |   $$ |  $$ |
+       ymymmy/:msdh  dsdy./dhhmh+	$$ | \$$\    $$ |   \$$$$$$  |
+        hhohmdymsmh  msmhymysddo 	\__|  \__|   \__|    \______/ 
+         smmhhdmsmd  msmmhhmmy+			
+           sdmdhohd  doyhmdy+-         Computer Engineering (Forever <3)
+              oyyhy  dhys+					
+
+ .-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.
+ |                                                                       |
+ |                 COPY-MOVE FORGERY DETECTION TOOL                      |
+ |            @AliEmreKeskin - @altunmustafa - @abdllhcay                |
+ |        github.com/AliEmreKeskin/Copy-Move-Forgery-Detection           |
+ |                                                                       |
+ .-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.
+
+"""
+
+os.system("cls")
+print(intro)
 
 #loading source image
-img=cv2.imread("img\TP_C01_006_copy.png",cv2.IMREAD_GRAYSCALE)
-height, width=img.shape[:2]
-cv2.imshow("Img",img)
-cv2.waitKey(1)
+img = cv2.imread(args.input, cv2.IMREAD_GRAYSCALE)
+height, width = img.shape[:2]
+#cv2.imshow("Input file",img)
+#cv2.waitKey(1)
+
+print("[+] Input file: {}".format(args.input))
+print("[+] Dimensions: {}x{}".format(width, height))
 
 #result=np.copy(img)
-result=np.zeros((height,width),np.uint8)
+result = np.zeros((height,width),np.uint8)
 
-CopyMoveDetect.BlockMatchDCT(img,result,8,0,16,0,2,50,150)
+start_time = time.time()
+CopyMoveDetect.BlockMatchDCT(img,result,args.blockSize,args.reduce,args.quantization,args.compareCount,args.differenceThreshold,args.shiftNormThreshold,args.shiftCountThreshold)
+end_time = time.time()
+elapsed_time = end_time - start_time
 
-cv2.imshow("result",result)
-cv2.waitKey(1)
+#cv2.imshow("result",result)
+#cv2.waitKey(1)
 
-cv2.imwrite("result.png",result)
+cv2.imwrite(args.output,result)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+print("[+] Done!")
+print("[+] Elapsed time: %.2f\n" %elapsed_time)
+
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
